b96ff70504daf6f5dfbdb8d278336a436d6f1163
KevinBaoB <kevinbaobui@gmail.com>
2022-05-26 13:13:52 -0500

    First

--- js/creditCheck.js
+++ js/creditCheck.js
@@ -1,2 +1,20 @@
-exports.creditCheck = function(str) {
-}
+exports.creditCheck = function (str) {
+  let arr = [];
+  str = str.toString().split("").reverse();
+  for (let i in str) {
+    if (i % 2 !== 0) {
+      arr.push(+str[i] * 2);
+    } else {
+      arr.push(+str[i]);
+    }
+  }
+  for (let j in arr) {
+    if (arr[j] > 9) {
+      let bigger = arr[j].toString().split("");
+      arr[j] = +bigger[0] + +bigger[1];
+    }
+  }
+  return arr;
+};
+
+console.log(this.creditCheck("5541808923795240"));
